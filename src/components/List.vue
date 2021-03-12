7b296c7bf2f78dc7dd4327d80ba1b2a283c56dc8
Osman Can Dülger <59533680+osmancandulger@users.noreply.github.com>
2021-03-12 06:16:05 +0300

    Added sort feature according to Status

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,5 +1,9 @@
 <template>
-  <div id="app">
+  <link
+    href="https://fonts.googleapis.com/icon?family=Material+Icons"
+    rel="stylesheet"
+  />
+  <div id="table">
     <!-- <table>
       <tr>
         <th>#</th>
@@ -20,16 +24,24 @@
         <th>#</th>
         <th>Title</th>
         <th>Assignee</th>
-        <th>Status</th>
+        <th class="status" @click="sort('completed')">
+          Status
+          <span class="material-icons">
+            arrow_drop_down
+          </span>
+        </th>
         <th>Actions</th>
       </tr>
-      <tr v-for="item in list" :key="item.id">
-        <td>{{ item.id }}</td>
+      <tr v-for="(item, i) in sortedList" :key="item.id">
+        <td>{{ i + 1 }}</td>
         <td>{{ item.title }}</td>
-
         <td>{{ item.name }}</td>
         <td v-if="item.completed">Done</td>
         <td v-else>In Progress</td>
+        <td>
+          <button class="edit-button">Edit</button>
+          <button class="delete-button" @click="remove()">Delete</button>
+        </td>
       </tr>
     </table>
   </div>
@@ -37,12 +49,19 @@
 
 <script>
 export default {
+  el: "#table",
+  data: {
+    list: [],
+    currentSortDir: "asc",
+  },
   data() {
     return {
       list: [],
       userList: [],
+      currentSortDir: "asc",
     };
   },
+
   created() {
     fetch("https://jsonplaceholder.typicode.com/todos")
       .then((response) => response.json())
@@ -56,6 +75,9 @@ export default {
   },
 
   methods: {
+    remove: function() {
+      console.log("...");
+    },
     bind: function() {
       for (let i = 0; i < this.list.length; i++) {
         for (let j = 0; j < this.userList.length; j++) {
@@ -64,6 +86,29 @@ export default {
           }
         }
       }
+    },
+
+    sort: function(s) {
+      if (s === this.currentSort) {
+        this.currentSortDir = this.currentSortDir === "asc" ? "desc" : "asc";
+      }
+      this.currentSort = s;
+      console.log("asd");
+      document.addEventListener("mousedown", function(e) {
+        e.preventDefault();
+      });
+    },
+    
+  },
+  computed: {
+    sortedList: function() {
+      return this.list.sort((a, b) => {
+        let modifier = 1;
+        if (this.currentSortDir === "desc") modifier = -1;
+        if (a[this.currentSort] < b[this.currentSort]) return -1 * modifier;
+        if (a[this.currentSort] > b[this.currentSort]) return 1 * modifier;
+        return 0;
+      });
     },
   },
 };
@@ -80,11 +125,36 @@ th {
   text-align: left;
   border-bottom: 2px solid rgba(214, 214, 214, 0.8);
 }
+.status {
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+}
+.status:hover {
+  color: blue;
+}
 tr td {
   text-align: left;
   padding-left: 15px;
 }
 td:nth-child(1) {
   width: fit-content;
+}
+.edit-button {
+  border: none;
+  border-radius: 5px;
+  color: #fff;
+  outline: none;
+  cursor: pointer;
+  background: rgb(65, 65, 241);
+}
+.delete-button {
+  border: none;
+  border-radius: 5px;
+  color: #fff;
+  outline: none;
+  cursor: pointer;
+  background: rgb(207, 9, 9);
+  margin-left: 5px;
 }
 </style>
